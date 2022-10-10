79d4e5551e335b16710e050a98be9917865be729
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-10 13:35:11 +0600

    show product on cart page done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CartContext } from './Root';
 
 const Header = () => {
+
+  const [cart,setCart] = useContext(CartContext);
+  
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
@@ -52,7 +56,7 @@ const Header = () => {
               title='Cart'
               className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
-              <p> Cart</p>
+              <p> Cart <sup>{cart.length}</sup></p>
             </Link>
           </li>
           <li>
