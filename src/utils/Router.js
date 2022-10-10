bab90bf48bed4f3d875624dba9bbd317de4b6a5f
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-10 11:51:03 +0600

    make root.jsx for layout

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter} from 'react-router-dom'
-import Home from '../components/Home';
+import Root from '../components/Root';
 
 export const router = createBrowserRouter([{
     path: '/',
-    element: <Home/>
+    element: <Root/>
 }]);
 
