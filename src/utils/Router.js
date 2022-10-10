cbfc5e5e968f40cf3bfcc8077336e2399296eab6
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-10 11:56:17 +0600

    Routes added on router file

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,8 +1,29 @@
 import { createBrowserRouter} from 'react-router-dom'
 import Root from '../components/Root';
+import Home from '../components/Home';
+import Shop from '../components/Shop';
+import Cart from '../components/Cart';
+import About  from '../components/About';
 
 export const router = createBrowserRouter([{
     path: '/',
-    element: <Root/>
+    element: <Root/>,
+    children:[
+        {
+            path:'/', element:<Home/>
+        },
+        {
+            path:'/home', element:<Home/>
+        },
+        {
+            path:'/shop', element:<Shop/>
+        },
+        {
+            path:'/cart', element:<Cart/>
+        },
+        {
+            path:'/about', element:<About/>
+        }
+    ],
 }]);
 
